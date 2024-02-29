78723a59b83e4120a81e11c3d41356f48d1d015e
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2024-02-29 22:57:03 +0300

    Make Modification

--- Week 6/blogger/blog/models.py
+++ Week 6/blogger/blog/models.py
@@ -1,16 +1,14 @@
 from django.db import models
-from django.utils import timezone
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length =250)
-    body = models.TextField()
-    publish = models.DateTimeField(default = timezone.now)
-    created = models.DateTimeField(auto_now_add = True)
-    updated = models.DateTimeField(auto_now = True)
-    class Meta:
-        ordering = ['-publish']
-    
-    
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='blog_images/')
+    tags = models.CharField(max_length=255)  
+
+    def get_tags_list(self):
+        return [tag.strip() for tag in self.tags.split(',')]
+
     def __str__(self):
         return self.title
