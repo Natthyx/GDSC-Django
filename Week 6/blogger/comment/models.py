3387776aa1b160a3f95f90562d8a6309afad4da9
Natannan Zeleke <99822269+Natthyx@users.noreply.github.com>
2024-02-29 22:58:52 +0300

    Make Modification

--- Week 6/blogger/comment/models.py
+++ Week 6/blogger/comment/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from blog.models import Post
 
 class Comment(models.Model):
-    user = models.CharField(max_length=200)
     content = models.TextField()
-    created = models.DateTimeField(auto_now_add = True)
-    updated = models.DateTimeField(auto_now = True)
-    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=255)
+    published_date = models.DateTimeField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f"{self.user} - {self.content}"
+        return f'{self.author} - {self.content}'
